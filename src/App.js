60d99f08ab85df18816f5891da0d5cf539ef2676
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-03-12 21:23:28 +0100

    useCurrency hook created

--- src/App.js
+++ src/App.js
@@ -7,43 +7,16 @@ import ExchangeForm from "./ExchangeForm";
 import Footer from "./Footer";
 import currencyTable from "./currency";
 import { Clock } from "./Clock";
+import { useCurrency } from "./useCurrency";
 
 function App() {
-  const [inputCurrency, setInputCurrency] = useState("PLN");
-  const [outputCurrency, setOutputCurrency] = useState("USD");
-
-  const calculateRate = () => {
-    const inputRate = currencyTable.find(
-      (curr) => curr.short === inputCurrency
-    ).toPLN;
-    const outputRate = currencyTable.find(
-      (curr) => curr.short === outputCurrency
-    ).toPLN;
-
-    return (inputRate / outputRate).toFixed(2);
-  };
-
-  const changeCurrencyInput = (id) => {
-    setInputCurrency(
-      (inputCurrency) =>
-        (inputCurrency = currencyTable.find((curr) => curr.id == id).short)
-    );
-  };
-
-  const changeCurrencyOutput = (id) => {
-    setOutputCurrency(
-      (outputCurrency) =>
-        (outputCurrency = currencyTable.find((curr) => curr.id == id).short)
-    );
-  };
-
-  const onInputChange = ({ target }) => {
-    changeCurrencyInput(target.value);
-  };
-
-  const onOutputChange = ({ target }) => {
-    changeCurrencyOutput(target.value);
-  };
+  const {
+    inputCurrency,
+    outputCurrency,
+    calculateRate,
+    onInputChange,
+    onOutputChange,
+  } = useCurrency(currencyTable);
 
   return (
     <>
---
+++ src/useCurrency.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export const useCurrency=(currencyTable)=>{
+  const [inputCurrency, setInputCurrency] = useState("PLN");
+  const [outputCurrency, setOutputCurrency] = useState("USD");
+
+  const calculateRate = () => {
+    const inputRate = currencyTable.find(
+      (curr) => curr.short === inputCurrency
+    ).toPLN;
+    const outputRate = currencyTable.find(
+      (curr) => curr.short === outputCurrency
+    ).toPLN;
+
+    return (inputRate / outputRate).toFixed(2);
+  };
+
+  const changeCurrencyInput = (id) => {
+    setInputCurrency(
+      (inputCurrency) =>
+        (inputCurrency = currencyTable.find((curr) => curr.id == id).short)
+    );
+  };
+
+  const changeCurrencyOutput = (id) => {
+    setOutputCurrency(
+      (outputCurrency) =>
+        (outputCurrency = currencyTable.find((curr) => curr.id == id).short)
+    );
+  };
+
+  const onInputChange = ({ target }) => {
+    changeCurrencyInput(target.value);
+  };
+
+  const onOutputChange = ({ target }) => {
+    changeCurrencyOutput(target.value);
+  };
+
+  return {inputCurrency,outputCurrency,calculateRate,onInputChange,onOutputChange}
+}
